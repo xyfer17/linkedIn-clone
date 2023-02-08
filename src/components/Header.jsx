d43549aeab128e0095b87fb37db735a24f69588f
xyfer17 <naveen@feldspartech.com>
2023-02-08 14:03:05 +0530

    header changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,33 +4,80 @@ import styled from 'styled-components'
 const Header = (props) => {
   return (
     <Container>
-        <Content>
-          <Logo>
-            <a href="/">
-              <img src="/images/home-logo.svg" alt="" />
-            </a>
-          </Logo>
-          <Search>
-            <div>
-              <input type="text" placeholder='Search' />
-            </div>
-            <SearchIcon>
+      <Content>
+        <Logo>
+          <a href="/home">
+            <img src="/images/home-logo.svg" alt="" />
+          </a>
+        </Logo>
+        <Search>
+          <div>
+            <input type="text" placeholder='Search' />
+          </div>
+          <SearchIcon>
             <img src="/images/search-icon.svg" alt="" />
-            </SearchIcon>
-          </Search>
-          <Nav>
-            <NavlistWrap>
-              <Navlist>
-                <a href="#">
-                  <img src="/images/nav-home.svg" alt="" />
-                </a>
-              </Navlist>
-            </NavlistWrap> 
-            </Nav>
-        </Content>
+          </SearchIcon>
+        </Search>
+        <Nav>
+          <NavlistWrap>
+            <Navlist className='active'>
+              <a href="/">
+                <img src="/images/nav-home.svg" alt="" />
+                <span>Home</span>
+              </a>
+            </Navlist>
+            <Navlist>
+              <a href="/">
+                <img src="/images/nav-network.svg" alt="" />
+                <span>My Network</span>
+              </a>
+            </Navlist>
+            <Navlist>
+              <a href="/">
+                <img src="/images/nav-jobs.svg" alt="" />
+                <span>Jobs</span>
+              </a>
+            </Navlist>
+            <Navlist>
+              <a href="/">
+                <img src="/images/nav-messaging.svg" alt="" />
+                <span>Messaging</span>
+              </a>
+            </Navlist>
+            <Navlist>
+              <a href="/">
+                <img src="/images/nav-notifications.svg" alt="" />
+                <span>Notifications</span>
+              </a>
+            </Navlist>
+            <User>
+            <a href="/">
+                <img src="/images/user.svg" alt="" />
+                <span>Me</span>
+                <img src="/images/down-icon.svg" alt="" />
+              </a>
+              <SignOut>
+                <a href="/">Sign Out</a>
+              </SignOut>
+            </User>
+            <Work>
+              <a href="/">
+                <img src="/images/nav-work.svg" alt="" />
+                <span>
+                  Work
+                  <img src="/images/down-icon.svg" alt="" />
+                </span>
+              </a>
+            </Work>
+          </NavlistWrap>
+        </Nav>
+      </Content>
     </Container>
   )
 }
+
+
+
 
 
 const Logo = styled.span`
@@ -41,12 +88,58 @@ const Logo = styled.span`
 const Navlist = styled.li`
 display:flex;
 align-items:center;
+ a{
+  align-items:center;
+  background:transparent;
+  display:flex;
+  flex-direction:column;
+  font-size:12px;
+  font-weight:400;
+  justify-content:center;
+  line-height:1.5;
+  min-height:52px;
+  min-width:80px;
+  position:relative;
+  text-decoration:none;
+
+    span{
+      color: rgba(0,0,0,0.6);
+      display:flex;
+      align-items:center;     
+
+    }
+    @media (max-width:768px) {
+      min-width:78px;
+    }
+ }
+
+ &:hover, &:active{
+  a{
+    span{
+      color:rgba(0,0,0,0.9);
+    }
+  }
+ }
+
 `
 
 const NavlistWrap = styled.ul`
 display:flex;
 flex-wrap:nowrap;
 list-style-type:none;
+.active{
+  span:after{
+      content:"";
+      transform:scaleX(1);
+      border-bottom: 2px solid var(--white,#fff);
+      bottom:0;
+      left:0;
+      position:absolute;
+      transition: transform 0.2s  ease-in-out;
+      width:100%;
+      border-color:rgba(0,0,0,0.9);
+  }
+}
 `
 
 const Search = styled.div`
@@ -119,5 +212,49 @@ margin:0 auto;
 min-height: 100%;
 max-width:1128px;
 `
+
+const SignOut = styled.div`
+  position:absolute;
+  top:45px;
+  background:white;
+  border-radius: 0 0 5px  5px;
+  width:100px;
+  height:40px;
+  font-size:16px;
+  font-weight:300;
+  transition-duration:  167ms;
+  text-align:center;
+  display:none;
+`
+
+
+const User = styled(Navlist)`
+ a > img{
+  width:24px;
+  height:24px;
+  border-radius:50%;
+ }
+
+ span{
+  display:flex;
+  align-items:center;
+ }
+
+ &:hover{
+  ${SignOut}{
+  align-items:center;
+  display:flex;
+  justify-content:center;
+}
+ }
+  
+`
+
+const Work = styled(User)`
+  border-left: 1px solid rgba(0,0,0,0.08);
+
+`
+
+
 
 export default Header
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,8 +6,17 @@ const Home = (props) => {
   return (
     <div>
         <Header/>
+        <Container>
+          <Section>
+            <h5><a href='/'>Hiring in a Hurry? --</a></h5>
+          </Section>
+        </Container>
     </div>
   )
 }
+
+
+const  Container = styled.div``
+const  Section = styled.section``
 
 export default Home
