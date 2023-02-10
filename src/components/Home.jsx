b64aa5b0c64cf96638648b9d0e7c828921e24eee
xyfer17 <naveen@feldspartech.com>
2023-02-11 00:33:39 +0530

    add left side

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,87 @@
 import React from 'react'
 import styled from "styled-components";
 import Header from './Header';
+import LeftSide from './LeftSide';
+import Main from './Main';
+import RightSide from './RightSide';
 
 const Home = (props) => {
   return (
     <div>
-        <Header/>
-        <Container>
+      <Header />
+      <Container>
+        
           <Section>
             <h5><a href='/'>Hiring in a Hurry? --</a></h5>
+            <p>Find Talendted Pros in record time with Upwork</p>
           </Section>
-        </Container>
+          <Layout>
+            <LeftSide />
+            <Main />
+            <RightSide />
+          </Layout>
+
+        
+
+
+
+
+      </Container>
     </div>
   )
 }
 
+const Layout = styled.div`
+display: grid;
+grid-template-areas: "leftside main rightside";
+grid-template-columns:minmax(0, 5fr) minmax(0,12fr) minmax(300px,7fr);
+column-gap:25px;
+row-gap:25px;
+margin:25px 0;
 
-const  Container = styled.div``
-const  Section = styled.section``
+@media (max-width:768px) {
+  display:flex;
+  flex-direction:column;
+  padding: 0 5px;
+}
+`
+
+const Container = styled.div`
+  padding-top:52px;
+  max-width:100%;
+`
+const Section = styled.section`
+  min-height:50px;
+  padding:16px 0;
+  box-sizing:content-box;
+  text-align:center;
+  text-decoration:underline;
+  display:flex;
+  justify-content:center;
+  h5{
+     color:#0a66c2;
+     font-size:14px;
+     a{
+      font-weight:700;
+     }
+  }
+
+  p{
+    font-size:14px;
+    color:#434649;
+    font-weight:600;
+  }
+
+  @media (max-width:786px) {
+      flex-direction:column;
+      padding:0 5px;
+    
+  }
+`
+const Content = styled.div`
+    max-width:1128px;
+    margin-left:auto;
+    margin-right:auto;
+`
 
 export default Home
---
+++ src/components/LeftSide.jsx
@@ -0,0 +1,98 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const LeftSide = () => {
+  return (
+    <Container>
+      <ArtCard>
+        <UserInfo>
+          <CardBackground/>
+          <a>
+            <Photo/>
+            <Link>Welcome, there!</Link>
+          </a>
+          <a>
+            <AddPhotoText>Add a Photo</AddPhotoText>
+          </a>
+        </UserInfo>
+        <Widget>
+          <a>
+            <span>Connection</span>
+            <span>Grow your network</span>
+          </a>
+        </Widget>
+        </ArtCard>
+    </Container>
+  )
+}
+
+
+const UserInfo = styled.div`
+    border-bottom:1px solid rgba(0,0,0,0.15);
+    padding: 12px 12px 16px;
+    word-wrap: break-word;
+    word-break:break-word;
+   
+`
+
+const CardBackground = styled.div`
+    background:url('/images/card-bg.svg');
+    background-position:center;
+    background-size:462px;
+    height:54px;
+    margin:-12px -12px 0;
+    
+    
+`
+
+const Photo = styled.div`
+    box-shadow:none;
+    background-image:url('/images/photo.svg');
+    width:72px;
+    height:72px;
+    box-sizing:border-box;
+    background-color:white;
+    background-clip:content-box;
+    background-position:center;
+    background-size:60%;
+    background-repeat:no-repeat;
+    border:2px solid white;
+    margin: -38px auto 12px;
+    border-radius:50%;
+
+`
+
+const Link = styled.div`
+    font-size:16px;
+    line-height:1.5;
+    color:rgba(0,0,0,0.9);
+    font-weight:600;
+`
+
+const AddPhotoText = styled.div`
+    color:#0a66c2;
+    margin-top:4px;
+    font-size:12px;
+    line-height:1.33;
+    font-weight:400;
+`
+
+const ArtCard = styled.div`
+  text-align:center;
+  overflow:hidden;
+  margin-bottom:8px;
+  background-color:#fff;
+  border-radius:5px;
+  transition:box-shadow 83ms;
+  position:relative;
+  border:none;
+  box-shadow: 0 0 0 1px rgb(0 0 0 / 15%), 0 0 0 rgb(0 0 0 / 20%);
+`
+
+const Container = styled.div`
+  grid-area:leftside;
+`
+
+const Widget = styled.div``
+
+export default LeftSide
