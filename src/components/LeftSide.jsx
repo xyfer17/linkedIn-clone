694ce382d0850497b16e06935a03349f1d116eee
xyfer17 <naveen@feldspartech.com>
2023-02-13 00:04:04 +0530

    main page modified

--- src/components/LeftSide.jsx
+++ src/components/LeftSide.jsx
@@ -17,11 +17,41 @@ const LeftSide = () => {
         </UserInfo>
         <Widget>
           <a>
+            <div>
             <span>Connection</span>
             <span>Grow your network</span>
+            </div>
+             <img src="/images/widget-icon.svg" alt="" />
           </a>
         </Widget>
+        <Item>
+          <span>
+            <img src="/images/item-icon.svg" alt="" />
+            My Items
+          </span>
+        </Item>
         </ArtCard>
+        <CommunityCard>
+          <a>
+            <span>Groups</span>
+          </a>
+          <a>
+            <span>
+              Events
+              <img src="/images/plus-icon.svg" alt="" />
+            </span>
+          </a>
+          <a>
+            <span>
+              Follow HashTags
+            </span>
+          </a>
+          <a>
+            <span>
+              Discover More
+            </span>
+          </a>
+        </CommunityCard>
     </Container>
   )
 }
@@ -93,6 +123,98 @@ const Container = styled.div`
   grid-area:leftside;
 `
 
-const Widget = styled.div``
+const Widget = styled.div`
+  border-bottom: 1px solid rgba(0,0,0,0.15);
+  padding-top:12px;
+  padding-bottom:12px;
+  &>a{
+    text-decoration:none;
+    display:flex;
+    justify-content:space-between;
+    align-items:center;
+    padding: 4px 12px;
+  }
+
+    &:hover{
+      background-color: rgba(0,0,0,0.08);
+    }
+
+    div{
+      display:flex;
+      flex-direction:column;
+      text-align:left;
+      span{
+        font-size:12px;
+        line-height:1.333;
+        &:first-child{
+          color: rgba(0,0,0,0.6);
+        }
+        &:nth-child(2){
+          color:rgba(0,0,0,1);
+        }
+      }
+    
+
+  }
+
+  svg{
+    color:rgba(0,0,0,1);
+  }
+`
+
+const Item = styled.a`
+  border-color:rgba(0,0,0,0.8);
+  text-align:left;
+  padding:12px;
+  font-size:12px;
+  display:block;
+  span{
+    display:flex;
+    align-items:center;
+    color:rgba(0,0,0,1);
+    svg{
+      color:rgba(0,0,0,0.8);
+    }
+  }
+
+
+  &:hover{
+    background-color:rgba(0,0,0,0.08);
+  }
+
+`
+
+
+const CommunityCard = styled(ArtCard)`
+  padding:8px 0 0;
+  text-align:left;
+  display:flex;
+  flex-direction:column;
+  a{
+    color:black;
+    padding: 4px 12px 4px 12px;
+    font-size:12px;
+
+    &:hover{
+      color:#0a66c2;
+    }
+
+    span{
+      display:flex;
+      align-items:center;
+      justify-content:space-between;
+    }
+
+    &:last-child{
+      color:rgba(0,0,0,0.6);
+      text-decoration:none;
+      border-top: 1px solid #d6cec2;
+      padding:12px;
+      &:hover{
+        background-color:rgba(0,0,0,0.08);
+      }
+    }
+  }
+`
 
 export default LeftSide
